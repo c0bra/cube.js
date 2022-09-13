a9153780b851d6aef4fc75a59d23807a8796ce46
buntarb <buntarb@gmail.com>
2022-09-13 18:28:42 +0300

    wip

--- packages/cubejs-query-orchestrator/src/orchestrator/PreAggregations.ts
+++ packages/cubejs-query-orchestrator/src/orchestrator/PreAggregations.ts
@@ -1800,7 +1800,7 @@ export class PreAggregations {
       preAggregationsSchema: schema,
     });
     tables = tables.filter(row => `${schema}.${row.table_name}` === table);
-    return table.length === 1;
+    return tables.length === 1;
   }
 
   public loadAllPreAggregationsIfNeeded(queryBody) {
